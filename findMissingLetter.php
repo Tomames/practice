a8dbe1e89ccf9e5ecb93436db870706bc01dd0a5
Tom James <794599429@qq.com>
2017-12-29 00:56:10 +0800

    optimize

--- findMissingLetter.php
+++ findMissingLetter.php
@@ -1,12 +1,16 @@
 <?php 
 
-function find_missing_letter(array $array): string {
-	$char = array();
-	foreach ($array as $value) {
-		$char[] = ord($value);
-	}
+// function find_missing_letter(array $array): string {
+// 	$char = array();
+// 	foreach ($array as $value) {
+// 		$char[] = ord($value);
+// 	}
 
-	return chr(current(array_diff(range($char[0], $char[count($char)-1]),$char)));
+// 	return chr(current(array_diff(range($char[0], $char[count($char)-1]),$char)));
+// }
+//原来range()也可以生成字母数组的^.^
+function find_missing_letter(array $array): string {
+	return current(array_diff(range(current($array), end($array)),$array));
 }
 
 echo find_missing_letter(['a','b','c','d','f']);
